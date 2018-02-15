2ef824f271cc648bf6580ad43c2f579c6e34b85e
MatthewRoberts <Matt.roberts4000@gmail.com>
2018-02-15 20:07:35 +0000

    edited

--- api/config/config.js
+++ api/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env.PORT || 4200,
-    database: process.env.MONGODB_URI || 'mongodb://localhost:27017/poll-station'
+    database: process.env.MONGODB_URI || 'mongodb://localhost:27017/pollingdbname'
 };
